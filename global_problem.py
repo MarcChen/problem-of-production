00789cb5a2c77cc04f7c8a85b71fb5b3c18c52ef
Chen Marc wsl <marc.chen@ensta-paris.fr>
2023-06-02 11:42:05 +0200

    Implementation of some csv reading functions and calculation of mean and covariance

--- global_problem.py
+++ global_problem.py
@@ -1,14 +1,17 @@
 import pandas as pd
 import numpy as np 
-# import import_data
+import import_data 
 import cvxpy as cp
 from scipy.stats import norm
 
 ### Importing the DATA ### 
-
+'''
+datas = []
+datas = import_data.numpy_csv_reader-("filename")
+'''
 # TEMP/DEFAULT values #
 
-n = 50
+n = 2
 temp_c_bar = 1
 temp_c_max = np.ones((2*n,1))
 temp_mean_R = np.full((2*n,1), 0.3)
@@ -17,6 +20,7 @@ temp_mean_D = 350
 temp_sigma_D = 0.3 * temp_mean_D
 temp_epsilon = 0.1
 temp_cov_R = r = np.full((2*n,2*n), 0.5)
+
 
 def optimization(c_bar = temp_c_bar, c_max = temp_c_max, cov_R = temp_cov_R , mean_R = temp_mean_R , sigma_D = temp_sigma_D , epsilon = temp_epsilon,mean_D = temp_mean_D):
     # Quantile of standard normal distribution for the given epsilon
@@ -45,9 +49,9 @@ def optimization(c_bar = temp_c_bar, c_max = temp_c_max, cov_R = temp_cov_R , me
 
     return [c.value, problem.value]
 
-[c_result, inf_value] = optimization()
+#[c_result, inf_value] = optimization()
 
-print("c result = ", c_result, " and the value is ", inf_value)
+#print("c result = ", c_result, " and the value is ", inf_value)
 
 ### Solving the optimization problem ###
 
--- import_data.py
+++ import_data.py
@@ -1,17 +1,48 @@
-import pandas as pd 
 import numpy as np 
 
-### Importing the 
+### Importing the data ###
+
+def numpy_csv_reader(file_path, delimiter=',', dtype=float, skiprows=1):
+    data = np.loadtxt(file_path, delimiter=delimiter, dtype=dtype, skiprows=skiprows)
+    return 
 
 ### Data formating process ###
 
-#Example to calculate covariance 
-# Create a 2 x n matrix
-matrix = np.array([[1, 2, 3], [4, 5, 6]])
+def computing_mean(datas):
+    # Coverting to np arrays
+    datas = np.array(datas)
 
-# Reshape the matrix into a n x 2 DataFrame
-df = pd.DataFrame(matrix.T, columns=['A', 'B'])
+    # Compute the mean
+    mean = np.mean(datas, axis=0)
+    
+    return mean
 
-# Calculate the covariance matrix
-cov_matrix = df.cov()
+def compute_covariance_matrix(datas):
+    n = len(datas) // 2  # Compute the value of n based on the length of the vector
+
+    # Split the vector into wind and PV components
+    wind_component = datas[:n]
+    pv_component = datas[n:]
+
+    # Compute the covariance matrices
+    cov_wind = np.cov(wind_component)
+    cov_pv = np.cov(pv_component)
+    cov_wind_pv = np.cov(wind_component, pv_component)
+    cov_pv_wind = np.cov(pv_component, wind_component)
+
+    # Create the covariance matrix Cov(R)
+    cov_r = np.block([[cov_wind, cov_wind_pv], [cov_pv_wind, cov_pv]])
+
+    return cov_r
+
+
+def computing_RD_covariance(R_array, D_array):
+    
+    # Concatenate R and D horizontally
+    RD = np.hstack((R_array, D_array))
+    
+    # Compute the covariance matrix for RD
+    RD_covariance = np.cov(RD, rowvar=False)
+    
+    return RD_covariance
 
